7473a742ec8a1cd3b79eaa670ce330d9d13fcd0f
Wafe Saleh <wafasaleh@confledis.com>
2020-06-12 01:04:34 +0200

    Add the routing of the page

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   public appPages = [
     {
       title: 'Home',
-      url: '/folder/home',
+      url: '/home',
       icon: 'home'
     },
     {
