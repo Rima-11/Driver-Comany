48cbbeb7b243fe99fae8670ce35ce5547111192e
Sabrine BEANABDALLAH <sabrinebenabdallah@confledis.com>
2020-06-22 22:54:48 +0100

    Add new page create-account

--- projects/app/src/app/create-account/create-account.page.ts
+++ projects/app/src/app/create-account/create-account.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ViewChildren, QueryList } from '@angular/core';
+import {HttpClient} from'@angular/common/http';
+import {IonSlides, NavController} from '@ionic/angular';
+import {FormBuilder, FormGroup} from '@angular/forms'
+import {Validators,ValidatorFn, AbstractControl} from '@angular/forms'
+
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +12,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('signupSlider') signupSlider;
+  submitAttempt: boolean;
+  slideOneForm: any;
+  slides: any;
+  store: any;
+ ngOnInit() {
+  
+  }
+  constructor(public nav: NavController,public formBuilder: FormBuilder,private http:HttpClient) {
 
-  ngOnInit() {
+    this.slideOneForm = formBuilder.group({
+        firstName: ['', Validators.compose([Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        lastName: ['', Validators.compose([Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        email: ['',Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ])],
+        phone:['',Validators.compose([
+          Validators.required])],
+        accountType:['',Validators.compose([
+          Validators.required])],
+        code:['',Validators.compose([
+            Validators.required])],
+        zip :['',Validators.compose([
+          Validators.required])],
+        town:['',Validators.compose([
+          Validators.required])],
+        country:['',Validators.compose([
+          Validators.required])],
+        password: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+        Confirmpassword:['',[Validators.required,this.equalto('password')]]
+        // person:['',]
+    });
+}
+
+//password 
+
+equalto(field_name): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} => {
+  
+  let input = control.value;
+  
+  let isValid=control.root.value[field_name]==input
+  if(!isValid) 
+  return { 'equalTo': {isValid} }
+  else 
+  return null;
+  };
   }
 
+//previous next
+
+
+
+next(){
+  this.signupSlider.slideNext();
+}
+checkValue(event){ 
+  console.log(event.detail.value);
+}
+prev(){
+  this.signupSlider.slidePrev();
+}
+
+//create account
+  save(){
+
+    this.submitAttempt = true;
+
+    if(!this.slideOneForm.valid){
+        this.signupSlider.slideTo(0);
+    }
+    else {
+       let url="http://localhost:3000/users";
+  this.http.post(url,this.slideOneForm.value).toPromise().then((data:any)=>{
+    console.log(data);
+  }) 
+  console.log(this.slideOneForm.value.phone);
+    }   
+}
 }
