d02cc0a03e603cf6d404318162c0fcce31d30968
Sabrine BEANABDALLAH <sabrinebenabdallah@confledis.com>
2020-07-10 18:44:21 +0100

    Add db.json

--- projects/app/src/app/edit/edit.page.ts
+++ projects/app/src/app/edit/edit.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Profile } from '../models/profile';
+import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router'; 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.page.html',
   styleUrls: ['./edit.page.scss'],
 })
 export class EditPage implements OnInit {
-
-  constructor() { }
+  id: number;
+  data: Profile;
+  constructor(
+    public apiService: ApiService,
+    public router: Router
+  ) {
+    this.data = new Profile();
+  }
 
   ngOnInit() {
+  }
+
+  update() {
+    //Update item by taking id and updated data object
+    this.apiService.updateItem(this.id, this.data).subscribe(response => {
+      console.log(this.data);
+    })
   }
 
 }
