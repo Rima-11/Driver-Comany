1a8cd4736c7488e6ecddb6e1b50f47f24ed3dfdd
Sabrine Ben <sabrinebenabdallah@confledis.com>
2020-07-22 14:30:46 +0100

    edit password

--- projects/app/src/app/password/password.page.ts
+++ projects/app/src/app/password/password.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../models/profile';
+import { ApiService } from '../services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.page.html',
   styleUrls: ['./password.page.scss'],
 })
+
+
 export class PasswordPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  profilesData: any;
+  id: number;
+  user: Profile;
+  password:string;
+  passwordForm: any;
+  constructor(public apiService: ApiService, public activatedRoute: ActivatedRoute,public formBuilder: FormBuilder,private http:HttpClient, private router: Router) { 
+    this.user = new Profile;
   }
 
+  ngOnInit() {
+    this.passwordForm = this.formBuilder.group({
+      password: ['', Validators.compose([Validators.required])],
+      newPassword:['', Validators.compose([Validators.required])],
+    Confirmpassword:['',[Validators.required]]
+    })
+  }
+ verifyPassword(){
+if(this.passwordForm.password.value === this.user.password)
+{
+console.log(this.passwordForm.password.value);
+console.log(this.user.password)
+}
+ }
 }
